50bf96ed8cfb198e21881a53e163bc0d3165e4d4
Alejandro Linan <jandrolinan@gmail.com>
2022-05-04 12:22:39 -0500

    completed technical assessment

--- javascript/src/app/charge-service.js
+++ javascript/src/app/charge-service.js
@@ -11,20 +11,47 @@ class ChargeService {
       if (billStatement[chargeInfo.name]) {
         billStatement[chargeInfo.name] += 1;
       } else {
-        billStatement[chargeInfo.name] = 0;
+        billStatement[chargeInfo.name] = 1;
       }
     });
 
     Object.entries(billStatement).forEach(entry => {
       const [chargeName, chargeCount] = entry;
 
-      if (chargeCount >= 1) {
+      if (chargeCount > 1) {
         recurringCharges[chargeName] = chargeCount
       }
     });
 
     return recurringCharges;
   }
+
+  getRecurringChargesWithCost(){
+    let recurringCharges = this.getRecurringCharges();
+    let result = {};
+
+    Object.entries(recurringCharges).forEach(entry => {
+      const [chargeName, chargeCount] = entry;
+
+      StatementApi.getCharges().forEach(chargeInfo => {
+        if(chargeInfo.name == chargeName){
+          result[chargeName] = {
+            recurringAmount: chargeCount,
+            monthlyCost: chargeInfo.amount
+          }
+        }
+      });
+    });
+  }  
 }
 
 export default ChargeService;
+
+// { "spotify": 4 }
+
+// {
+//   "Spotify": {
+//     recurringAmount: 4,
+//     monthlyCost: 7.99
+//   }
+// }
